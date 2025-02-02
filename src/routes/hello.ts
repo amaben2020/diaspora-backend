9841337ce47e73dd866e9412be5525a32f52ba5b
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-02 17:37:47 +0100

    cleanup

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -6,11 +6,6 @@ const router = express.Router();
 router
   .route('/protected-auth-required')
   .get(ClerkExpressRequireAuth(), (req: Request, res: Response) => {
-    /* #swagger.security = [{
-            "apiKeyAuth": []
-    }] */
-
-    console.log(req);
     res.json(req.auth);
   });
 
