d529e42376b5d9b461d15feac89fbf339a26f695
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-02 19:48:23 +0100

    ading types

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -1,12 +1,16 @@
 import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
-import express from 'express';
+import express, { type Response } from 'express';
+import type { AuthenticatedRequest } from '../../types/globals';
 
 const router = express.Router();
 
 router
   .route('/protected-auth-required')
-  .get(ClerkExpressRequireAuth(), (req: Request, res: Response) => {
-    res.json(req.auth);
-  });
+  .get(
+    ClerkExpressRequireAuth(),
+    (req: AuthenticatedRequest, res: Response) => {
+      res.json(req.auth);
+    }
+  );
 
 export default router;
--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="@clerk/express/env" />
 
-interface authRequest<ReqBody = { user: IUser }> extends Request {}
+import { Request } from 'express';
+import { ClerkExpressRequireAuth } from '@clerk/express';
+
+declare interface AuthenticatedRequest extends Request {
+  auth: NonNullable<Request['auth']>;
+}
+
+export { AuthenticatedRequest };
