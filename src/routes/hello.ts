c5b33d1e4eae4db9f98c77a73d51c19e853916fb
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-03 21:58:10 +0100

    upd

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -4,14 +4,6 @@ import express, { type Response } from 'express';
 
 const router = express.Router();
 
-router
-  .route('/protected-auth-required')
-  .get(
-    ClerkExpressRequireAuth(),
-    (req: AuthenticatedRequest, res: Response) => {
-      console.log('Auth', req.auth);
-      res.json(req.auth);
-    },
-  );
+router.route('/protected-a 
 
 export default router;
