f301aeb78098da6b4848167ecfea987350fae3b1
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-04 18:19:21 +0100

    cleanup

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -1,53 +1,29 @@
 import type { AuthenticatedRequest } from '@/types/globals';
-import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
+import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express';
 import express, { type Response } from 'express';
+import { logger } from '../utils/logger.ts';
 
 const router = express.Router();
 
 router
   .route('/protected-auth-required')
-  .get(
-    ClerkExpressRequireAuth(),
-    (req: AuthenticatedRequest, res: Response) => {
-      console.log('Auth', req.auth);
-      res.json({ user: req.auth, name: 'Ben' });
-    },
-  );
+  .get(clerkMiddleware(), (req: AuthenticatedRequest, res: Response) => {
+    console.log('Auth', req.auth);
+    res.json({ user: req.auth, name: 'Ben' });
+  });
 
-router.route('/').get((_: AuthenticatedRequest, res: Response) => {
-  res.json({ message: 'Running' });
+router.route('/protected').get(requireAuth(), async (req, res) => {
+  const data = await getAuth(req);
+
+  console.log(req.auth);
+  logger.info(req.auth);
+  res.status(201).json({ data, auth: req.auth });
 });
 
+router
+  .route('/')
+  .get(clerkMiddleware(), (req: AuthenticatedRequest, res: Response) => {
+    res.json({ message: 'Running', auth: req.auth });
+  });
+
 export default router;
-
-//import {
-//   ClerkExpressRequireAuth,
-//   ClerkExpressWithAuth,
-// } from '@clerk/clerk-sdk-node';
-// import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express';
-// All routes have access to the auth based on token
-// app.get('/api/protected', (req, res) => {
-//   if (!req.auth.userId) {
-//     console.log('req.auth.userId', req.auth.userId);
-//     return res.status(401).json({ message: 'Unauthorized' });
-//   }
-
-//   res.json({ message: 'Protected route', user: req.auth });
-//   console.log(req.auth);
-// });
-
-// app.get('/protected', requireAuth(), (req, res) => {
-//   res.send('Protected data');
-//   console.log(req.auth);
-//   logger.info(req.auth);
-// });
-
-// app.get('/user', requireAuth(), (req, res) => {
-//   const user = req.auth;
-//   console.log('user', user);
-//   res.json({ user });
-// });
-
-// app.get('/', (req, res) => {
-//   res.send('Running');
-// });
