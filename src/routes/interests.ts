308e5b0f92c31882e7ff723ec255e64da250c646
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-05 17:35:23 +0100

    chore: refactor

--- src/routes/interests.ts
+++ src/routes/interests.ts
@@ -10,7 +10,6 @@ router.route('/interests').get((_, res: Response) => {
     logger.error('Interests not found');
   }
   res.json({ interests: INTERESTS });
-  logger.log({ level: '4', message: 'Success' });
 });
 
 export default router;
