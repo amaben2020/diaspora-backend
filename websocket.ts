cd4f8f729a60cffacadce9bc4a57d712e113631b
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-03-18 18:31:05 +0100

    chore: cleanups websocket

--- websocket.ts
+++ websocket.ts
@@ -6,6 +6,33 @@ import { userActivityTable } from './src/schema/userActivityTable.ts';
 export const ably = new Ably.Realtime({ key: process.env.ABLY_API_KEY });
 const channel = ably.channels.get('user-presence');
 
+// export async function updateUserStatus(userId: string, onlineStatus: boolean) {
+//   try {
+//     const [userOnlineStatus = undefined] = await db
+//       .select()
+//       .from(userActivityTable)
+//       .where(eq(userActivityTable.userId, userId));
+
+//     if (!userOnlineStatus) {
+//       await db.insert(userActivityTable).values({
+//         userId,
+//         onlineStatus,
+//         lastActive: new Date(),
+//       });
+//     } else {
+//       await db
+//         .update(userActivityTable)
+//         .set({ onlineStatus, lastActive: new Date() })
+//         .where(eq(userActivityTable.userId, userId));
+//     }
+
+//     // Publish status update to Ably
+//     await channel.publish('onlineStatusUpdated', { userId, onlineStatus });
+//   } catch (error) {
+//     console.error('Database error:', error);
+//   }
+// }
+
 export async function updateUserStatus(userId: string, onlineStatus: boolean) {
   try {
     const [userOnlineStatus = undefined] = await db
