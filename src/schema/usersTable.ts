4a8f060115df165ac705a50b8272d7846a9c112d
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-05-04 11:13:48 +0100

    feat: updating preferences schema

--- src/schema/usersTable.ts
+++ src/schema/usersTable.ts
@@ -1,32 +1,3 @@
-// import {
-//   boolean,
-//   date,
-//   pgTable,
-//   text,
-//   timestamp,
-//   varchar,
-// } from 'drizzle-orm/pg-core';
-
-// export const usersTable = pgTable('users', {
-//   id: text('id').primaryKey(),
-//   displayName: varchar('display_name', { length: 50 }),
-//   email: text('email').unique(),
-//   gender: varchar('gender', { length: 20 }),
-//   birthday: date('birthday'),
-//   verified: boolean('verified').default(false),
-//   showGender: boolean('show_gender').default(false),
-//   lastLogin: timestamp('last_login', { withTimezone: true }),
-//   subscriptionType: varchar('subscription_type', { length: 20 }).default(
-//     'free',
-//   ),
-//   phone: varchar('phone', { length: 11 }),
-//   createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
-//   updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-// });
-
-// export type InsertUser = typeof usersTable.$inferInsert;
-// export type SelectUser = typeof usersTable.$inferSelect;
-
 import {
   boolean,
   date,
